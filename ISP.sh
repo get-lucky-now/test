b5729bec3878d23539179099288cd8f15b7dcf76
get-lucky-now <bixbit.ceo@gmail.com>
2025-05-19 21:35:48 +0700

    Update ISP.sh

--- ISP.sh
+++ ISP.sh
@@ -78,7 +78,8 @@ systemctl restart sshd.service
 
 systemctl disable --now chronyd
 
-echo 'NTP=172.16.4.2' >> /etc/systemd/timesyncd.conf
+#echo 'NTP=172.16.4.2' >> /etc/systemd/timesyncd.conf
+sed -i 's|^#NTP=.*|NTP=172.16.4.2|' /etc/systemd/timesyncd.conf
 
 systemctl enable --now systemd-timesyncd
 systemctl restart systemd-timesyncd
