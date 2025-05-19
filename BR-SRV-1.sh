9331a2fadb8947bf0b7353ba5f95086a42941dd5
get-lucky-now <bixbit.ceo@gmail.com>
2025-05-19 20:34:55 +0700

    Update BR-SRV-1.sh

--- BR-SRV-1.sh
+++ BR-SRV-1.sh
@@ -55,8 +55,8 @@ wget https://raw.githubusercontent.com/get-lucky-now/script/main/BR-SRV-2.sh
 chmod +x BR-SRV-2.sh
 wget https://raw.githubusercontent.com/get-lucky-now/script/main/BR-SRV-3.sh
 chmod +x BR-SRV-3.sh
-wget https://raw.githubusercontent.com/get-lucky-now/script/main/import.sh
-chmod +x import.sh
+wget https://raw.githubusercontent.com/get-lucky-now/script/main/import
+chmod +x import
 
 #обновляем пакеты
 apt-get update
