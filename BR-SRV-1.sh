bc3567c358cbf54c7e46dc4558e1f6a980b48633
get-lucky-now <bixbit.ceo@gmail.com>
2025-05-19 15:17:49 +0700

    Update BR-SRV-1.sh

--- BR-SRV-1.sh
+++ BR-SRV-1.sh
@@ -39,10 +39,6 @@ systemctl restart sshd.service
 # Настраиаем timesyncd
 systemctl disable --now chronyd
 
-#cat <<EOF >> /etc/systemd/timesyncd.conf
-#NTP=172.16.4.2
-#EOF
-
 echo 'NTP=172.16.4.2' >> /etc/systemd/timesyncd.conf
 
 systemctl enable --now systemd-timesyncd
