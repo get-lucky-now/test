8fa844e93bad8f48d434cd9c8175569578e6f766
get-lucky-now <bixbit.ceo@gmail.com>
2025-05-19 16:39:30 +0700

    Update BR-RTR.sh

--- BR-RTR.sh
+++ BR-RTR.sh
@@ -113,5 +113,7 @@ echo 'NTP=172.16.4.2' >> /etc/systemd/timesyncd.conf
 
 systemctl enable --now systemd-timesyncd
 systemctl restart systemd-timesyncd
+timedatectl set-timezone Asia/Krasnoyarsk
+systemctl restart systemd-timesyncd
 timedatectl timesync-status
 exec bash
