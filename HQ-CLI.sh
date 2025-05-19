ed12e2f861dfe21601ffca3c96a018a886b8f268
get-lucky-now <bixbit.ceo@gmail.com>
2025-05-19 16:05:33 +0700

    Update HQ-CLI.sh

--- HQ-CLI.sh
+++ HQ-CLI.sh
@@ -4,7 +4,7 @@
 hostnamectl set-hostname hq-cli.au-team.irpo
 
 apt-get update
-apt-get install openssh-server systemd-timesyncd python-module-json -y
+apt-get install openssh-server systemd-timesyncd python-module-json yandex-browser-stable -y
 
 # Создаем нового пользователя
 useradd sshuser
